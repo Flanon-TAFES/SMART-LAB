eade60d3ae752268ab88ef530aabdddde062ac3d
Flanon-TAFES <105160355+Flanon-TAFES@users.noreply.github.com>
2022-10-23 00:08:43 +0900

    Update gpib_init.sh

--- gpib_init.sh
+++ gpib_init.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+# gpib config
+sudo modprobe ni_usb_gpib
+sudo /usr/local/sbin/gpib_config -t ni_usb_b
+sudo chmod 666 /dev/gpib0
+# gpib config end
+
+echo "GPIB initialization complete"
