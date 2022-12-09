494bc3464b99e83d8cfb2e8d3b25e342b39c8760
Flanon-TAFES <105160355+Flanon-TAFES@users.noreply.github.com>
2022-12-10 02:01:11 +0900

    Create lcd.h

---
+++ Firmware/lcd.h
@@ -0,0 +1,8 @@
+void LCD_Comm(char command);
+void LCD_Data(char ch);
+void LCD_PORT_Init(void);
+void LCD_Init(void);
+void LCD_Pos(unsigned char x, unsigned char y);
+void LCD_Char(unsigned char ch);
+void LCD_Str(unsigned char *str);
+void LCD_Clear(void);
